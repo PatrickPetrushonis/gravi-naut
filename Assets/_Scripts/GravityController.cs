c0a33daacc50764e57cf541f2f22c6a75075a66e
Patrick <pwp904@gmail.com>
2014-12-28 00:44:56 -0500

    Threshold gravity range, dynamic object prefab, player prefab

--- Assets/_Scripts/GravityController.cs
+++ Assets/_Scripts/GravityController.cs
@@ -4,15 +4,17 @@ using System.Collections;
 public class GravityController : MonoBehaviour 
 {
     private GameObject player;
+    private GameObject[] dynamic;
     private Gyroscope gyro;    
     //gravity
     public float gravity = 10f;
+    public float threshold = 0.6f;
     public Vector2 direction;
     //accelerometer
     private Vector2 currentAcc;
     private float sensitivityH = 5f;
     private float sensitivityV = 5f;
-    private float smooth = 0.5f;
+    private float smooth = 2.5f;
     private float getAxisH = 0f;
     private float getAxisV = 0f;
     //guistyle
@@ -21,6 +23,7 @@ public class GravityController : MonoBehaviour
     void Start()
     {
         player = GameObject.Find("Player");
+        dynamic = GameObject.FindGameObjectsWithTag("Dynamic");
 
         if(SystemInfo.supportsGyroscope)
         {
@@ -36,9 +39,9 @@ public class GravityController : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (gyro.enabled)
+        if(gyro.enabled)
         {
-            currentAcc = Vector2.Lerp(currentAcc, Input.acceleration, Time.deltaTime / smooth);
+            currentAcc = Vector2.Lerp(currentAcc, Input.acceleration, smooth * Time.deltaTime);
             getAxisH = Mathf.Clamp(currentAcc.x * sensitivityH, -1, 1);
             getAxisV = Mathf.Clamp(currentAcc.y * sensitivityV, -1, 1);
             direction = new Vector2(getAxisH, getAxisV);
@@ -53,10 +56,24 @@ public class GravityController : MonoBehaviour
     }
 
     void ApplyGravity()
-    {
-        //need certain range in which player can freely move without changing gravity
+    {        
+        float horizontal = Mathf.Abs(direction.x);
+        float vertical = Mathf.Abs(direction.y);
 
-        player.rigidbody2D.AddForce(direction * gravity);
+        //define range in which device can be oriented without affecting gravity
+        if((horizontal < threshold && vertical == 1) || (vertical < threshold && horizontal == 1))
+        {
+            return;
+        }
+        else 
+        {
+            player.rigidbody2D.AddForce(direction * gravity);
+
+            foreach(GameObject dynamicObj in dynamic)
+            {
+                dynamicObj.rigidbody2D.AddForce(direction * gravity);
+            }
+        }        
     }
 
     void ResetAcceleration()
