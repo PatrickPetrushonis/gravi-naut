aa40a04c91b001311d374e1e42648fe45ab53386
Patrick <pwp904@gmail.com>
2015-01-07 19:55:51 -0500

    Introduced collectible

---
+++ Assets/_Scripts/CollectOnTouch.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectOnTouch : MonoBehaviour 
+{
+    private Collider2D thisCollider;
+
+    void Start()
+    {
+        thisCollider = gameObject.GetComponent<Collider2D>();
+        
+        if(!thisCollider.isTrigger)
+        {
+            thisCollider.isTrigger = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
