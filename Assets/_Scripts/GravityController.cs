207667bf05232dd6f5b8b5d0e5f319834ec3b267
Patrick <pwp904@gmail.com>
2015-01-11 15:44:31 -0500

    Move to touch location function with gravity. Unable to determine if grounded.

--- Assets/_Scripts/GravityController.cs
+++ Assets/_Scripts/GravityController.cs
@@ -4,34 +4,32 @@ using System.Collections;
 public class GravityController : MonoBehaviour 
 {
     private GameObject player;
-    private GameObject[] dynamic;
-    private Gyroscope gyro;    
-    //gravity
-    public float gravity = 3f;
-    public float threshold = 0.95f;
-    public Vector2 direction;
-    private Vector2 initial = new Vector2(0, -1);
-    //velocity
-    private float speedLimit = 2;
-    private Vector2 playerVel;
-    private Vector2 oppDirect;
-    private Vector2 totalForce;
-    //accelerometer
-    private Vector2 currentAcc;
-    private float sensitivityH = 5f;
+    private GameObject[] dynamic;       //all non-player objects affected by gravity
+    private Gyroscope gyro;
+
+    public float gravity = 3f;          //force of gravity
+    private float threshold = 0.95f;    //minimum input required to effect gravity
+
+    public Vector2 direction;           //current direction of gravity
+    private float horizontal;           //absolute value for x and y axes of direction
+    private float vertical;              
+
+    private Vector2 playerVel;          //current player velocity
+    private float playerMag;            //magnitude of current velocity
+    private float speedLimit = 2;       //maximum total velocity
+    private float opposeForce;          //difference between current velocity magnitude and speed limit
+    private Vector2 opposeDir;          //opposite direction of current force of gravity
+    public Vector2 totalForce;          //total combination of all forces to act on player
+
+    private Vector2 currentAcc;         //current acceleration of device calculated with Lerp
+    private float sensitivityH = 5f;    //sensitivity of horizontal and vertical axes
     private float sensitivityV = 5f;
-    private float smooth = 2.5f;
-    private float getAxisH = 0f;
-    private float getAxisV = 0f;
-    //guistyle
-    private GUIStyle thisStyle = new GUIStyle();
+    private float smooth = 2.5f;        //smoothing factor in Lerp calculation
+    private float getAxisH = 0f;        //current horizontal and vertical axes input
+    private float getAxisV = 0f; 
 
     void Start()
     {
-        //GUIStyle for gravity direction notification
-        thisStyle.fontSize = 20;
-        thisStyle.normal.textColor = Color.white;   
-
         player = GameObject.Find("Player");
         dynamic = GameObject.FindGameObjectsWithTag("Dynamic");
 
@@ -39,91 +37,67 @@ public class GravityController : MonoBehaviour
         {
             gyro = Input.gyro;
             gyro.enabled = true;
-            ResetAcceleration();
+            currentAcc = Input.acceleration;
         }
 
-        InitialGravity();
+        //initial force of gravity
+        player.rigidbody2D.AddForce(new Vector2(0, -1) * gravity);
     }
 
     void FixedUpdate()
     {
-        //if(gyro.enabled)
-        {
-            DetermineDirection();
-        }
-    }
-
-    void OnGUI()
-    {
-        GUI.Label(new Rect(5, 25, 300, 25), "Gravity Direction (X, Y): " + direction, thisStyle);
+        DetermineDirection();
+        ApplyGravity(new Vector2(direction.x, direction.y));
     }
 
     void DetermineDirection()
     {
+        //device acceleration over time
         currentAcc = Vector2.Lerp(currentAcc, Input.acceleration, smooth * Time.fixedDeltaTime);
         getAxisH = Mathf.Clamp(currentAcc.x * sensitivityH, -1, 1);
         getAxisV = Mathf.Clamp(currentAcc.y * sensitivityV, -1, 1);
-        direction = new Vector2(getAxisH, getAxisV);
 
-        float horizontal = Mathf.Abs(direction.x);
-        float vertical = Mathf.Abs(direction.y);
+        //direction
+        direction = new Vector2(getAxisH, getAxisV);
+        horizontal = Mathf.Abs(direction.x);
+        vertical = Mathf.Abs(direction.y);        
 
         //define range in which device can be oriented without affecting gravity
         //portrait
         if(direction.y == -1 && horizontal < threshold)
-        {
             direction = new Vector2(0, -1);
-        }
         //upsidedown portrait
         else if(direction.y == 1 && horizontal < threshold)
-        {
             direction = new Vector2(0, 1);
-        }
         //left landscape
         else if(direction.x == -1 && vertical < threshold)
-        {
             direction = new Vector2(-1, 0);
-        }
         //right landscape
         else if(direction.x == 1 && vertical < threshold)
-        {
             direction = new Vector2(1, 0);
-        }
-
-        ApplyGravity(new Vector2(direction.x, direction.y));
     }
 
     void ApplyGravity(Vector2 direction)
     {
         playerVel = player.rigidbody2D.velocity;
-        oppDirect = Vector2.zero;
+        playerMag = playerVel.magnitude;
 
-        float playerMag = playerVel.magnitude;        
-        float oppForce = 0;
+        opposeDir = Vector2.zero;
+        opposeForce = 0;        
 
         if(playerMag > speedLimit)
         { 
-            //add force in opposing direction equal to exceeding value over speedLimit
-            oppDirect = -direction;
-            oppForce = playerMag - speedLimit;
+            //calculate difference between player magnitude and limit
+            opposeDir = -direction;
+            opposeForce = playerMag - speedLimit;
         }
 
-        totalForce = direction * gravity + oppDirect * oppForce;
-        player.rigidbody2D.AddForce(totalForce);
+        //overall force to be applied to character
+        //actual application is performed in CharacterController
+        totalForce = direction * gravity + opposeDir * opposeForce;
 
+        //to reduce calculations, non-player objects have no limit on velocity
         foreach(GameObject dynamicObj in dynamic)
-        {
             dynamicObj.rigidbody2D.AddForce(direction * gravity);
-        }                
-    }
-
-    void ResetAcceleration()
-    {
-        currentAcc = Input.acceleration;
-    }
-
-    void InitialGravity()
-    {
-        player.rigidbody2D.AddForce(initial * gravity);
     }
 }
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -5,91 +5,106 @@ public class PlayerController : MonoBehaviour
 {
     private GameObject gravityManager;
     private GravityController gravityController;
-    private Vector2 direction;
-    //force detection
-    private Vector2 previousVel;
-    private Vector2 currentVel;
-    private Vector2 currentForce;
-    //orientation
-    private float desiredAngle;
-    private float rotationSpeed = 45;
-    private float minChange = 0.1f;
+
+    private Vector2 direction;          //direction of gravity upon movement request
+    private Vector2 start;              //starting position during a movement request
+    private Vector2 end;                //destination for player movement
+    private bool moving = false;        //whether a movement request is being fulfilled
+
+    public float moveSpeed = 1.5f;      //speed of player movement
+    public float distanceToGround;      //distance to check if player is grounded
+    private float precision = 0.01f;    //maximum allowable offset from exact destination
 
     void Start()
     {
         gravityManager = GameObject.Find("GravityManager");
         gravityController = gravityManager.GetComponent<GravityController>();
+
+        end = transform.position;
+        distanceToGround = collider2D.bounds.extents.y + 0.1f;
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        GetAppliedForce();
+        //if(IsGrounded())
+        {
+            DetermineDestination();
+            if(moving)
+                MovePlayer();
+        }
 
-        //OrientationByGravityDirection();
-
-        //OrientationByAdjecentSurface();        
+        ApplyGravity();
     }
 
-    void GetAppliedForce()
+    bool IsGrounded()
     {
-        currentVel = rigidbody2D.velocity;
+        LayerMask mask = LayerMask.NameToLayer("Static");
 
-        if(currentVel != previousVel)
-        {
-            currentForce = currentVel - previousVel;
-        }
-        else 
-        {
-            currentForce = Vector2.zero;
-        }
-
-        previousVel = currentVel;
-    }
-
-    void OrientationByGravityDirection()
-    {
-        //get direction of gravity
-        direction = gravityController.direction;
-
-        float horizontal = Mathf.Abs(direction.x);
-        float vertical = Mathf.Abs(direction.y);
-        float threshold = 1f;
-        float desiredAngle = 0;
-
-        //define range in which device can be oriented without affecting gravity
-        //portrait
-        if(direction.y == -1 && horizontal < threshold)
-        {
-            desiredAngle = 0;
-        }
-        //upsidedown portrait
-        else if(direction.y == 1 && horizontal < threshold)
-        {
-            desiredAngle = 180;
-        }
-        //left landscape
-        else if(direction.x == -1 && vertical < threshold)
-        {
-            desiredAngle = -90;
-        }
-        //right landscape
-        else if(direction.x == 1 && vertical < threshold)
-        {
-            desiredAngle = 90;
-        }
-
-        //determine desired angle for player orientation
-        desiredAngle = Vector2.Angle(transform.position, direction);
-        Debug.Log(desiredAngle);
-
-        if(Mathf.Abs(transform.rotation.z - desiredAngle) > minChange)
-        {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, desiredAngle), Time.deltaTime * rotationSpeed);
-        }
-    }
-
-    void OrientationByAdjecentSurface()
-    { 
+        //cast ray down from current position to slightly beneath player
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, distanceToGround, mask);
         
+        Debug.DrawRay(transform.position, -Vector2.up, Color.red);
+
+        if(hit.collider != null)
+        {
+            Debug.DrawLine(transform.position, hit.point, Color.red);
+            Debug.Log(hit.transform.position.ToString());
+            return true;
+        }
+        
+        Debug.Log("Not grounded");
+        return false;
+    }
+
+    void DetermineDestination()
+    {
+        start = transform.position;
+
+        //Set target movement position to last touch position
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            end = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            moving = true;
+
+            //determines gravity direction
+            direction = gravityController.direction;
+
+            //limits movement to prevent undesired levitation
+            if(direction.y != 0 && direction.x < 1)
+                end.y = transform.position.y;
+            else if(direction.x != 0 && direction.y < 1)
+                end.x = transform.position.x;
+        }
+    }
+
+    void MovePlayer()
+    {
+        //to prevent jittering, check to see if current position is within acceptable bounds of end position
+        if(start.x > end.x - precision && start.x < end.x + precision)
+            if(start.y > end.y - precision && start.y < end.y + precision)
+            {
+                EndMovement();
+                return;
+            }
+
+        Vector2 moveDirection = (end - start).normalized;
+        rigidbody2D.velocity = moveDirection * moveSpeed;
+    }
+
+    void EndMovement()
+    {
+        end = start;
+        rigidbody2D.velocity = Vector2.zero;
+        moving = false;
+    }
+
+    void ApplyGravity()
+    {
+        rigidbody2D.AddForce(gravityController.totalForce);
+
+        //sudden change in gravity will prevent further movement
+        if(moving)
+            if(gravityController.direction.x != direction.x || gravityController.direction.y != direction.y)
+                EndMovement();
     }
 }
