7f3f70ac475818fb1972f90d85cf42b4fdb930ed
Patrick <pwp904@gmail.com>
2015-01-14 18:44:15 -0500

    Created follow camera with dampening. Reduced orthographic size.

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+    private Camera camera;
+    private GameObject player;
+    private Transform target;
+
+    private Vector3 velocity = Vector3.zero;
+    public float dampTime = 0.5f;
+    public float defaultSize = 1.95f;
+    public float pauseSize = 5f;    
+
+    void Start()
+    {
+        camera = Camera.main;
+        camera.orthographicSize = defaultSize;
+        player = GameObject.Find("Player");
+        target = player.transform;
+    }
+
+    void Update()
+    {
+        if(target)
+        {
+            Vector3 point = camera.WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        }
+    }
+}
